28635e67242551cb2966522eddfc8dc24e9d06bc
Benoit Patra <Benoit Patra@BenoitPatra-PC>
2011-08-04 15:04:33 +0200

    sealed class ProjectionComparer is in the same file than the extension class for collections

--- CloudDALVQ/Handy/ExtendIEnumerable.cs
+++ CloudDALVQ/Handy/ExtendIEnumerable.cs
@@ -95,4 +95,28 @@ namespace CloudDALVQ.Handy
             return result;
         }
 	}
+
+    sealed class ProjectionComparer<TValue, TProjection> : IEqualityComparer<TValue>
+    {
+        readonly Func<TValue, TProjection> _projection;
+
+        public ProjectionComparer(Func<TValue, TProjection> projection)
+        {
+            _projection = projection;
+        }
+
+        bool IEqualityComparer<TValue>.Equals(TValue x, TValue y)
+        {
+            var projectedX = _projection(x);
+            var projectedY = _projection(y);
+
+            return projectedX.Equals(projectedY);
+        }
+
+        int IEqualityComparer<TValue>.GetHashCode(TValue obj)
+        {
+            var projectedObj = _projection(obj);
+            return projectedObj.GetHashCode();
+        }
+    }
 }
--- CloudDALVQ/Handy/ProjectionComparer.cs
+++ CloudDALVQ/Handy/ProjectionComparer.cs
@@ -9,27 +9,5 @@ using System.Collections.Generic;
 
 namespace CloudDALVQ.Handy
 {
-	sealed class ProjectionComparer<TValue, TProjection> : IEqualityComparer<TValue>
-	{
-		readonly Func<TValue, TProjection> _projection;
-
-		public ProjectionComparer(Func<TValue, TProjection> projection)
-		{
-			_projection = projection;
-		}
-
-		bool IEqualityComparer<TValue>.Equals(TValue x, TValue y)
-		{
-			var projectedX = _projection(x);
-			var projectedY = _projection(y);
-
-			return projectedX.Equals(projectedY);
-		}
-
-		int IEqualityComparer<TValue>.GetHashCode(TValue obj)
-		{
-			var projectedObj = _projection(obj);
-			return projectedObj.GetHashCode();
-		}
-	}
+	
 }
